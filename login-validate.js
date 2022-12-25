9cd754c5ee198da35e7a195a46e9d800da2a808e
eri1on <121399005+eri1on@users.noreply.github.com>
2022-12-25 23:49:28 +0100

    Create login-validate.js

---
+++ login-validate.js
@@ -0,0 +1,22 @@
+function valido(){
+
+var email=document.getElementById('email').value
+
+if(email==('')){
+console.log(confirm('email cannot be null'));
+}
+
+
+var password=document.getElementById('password').value
+if(password==('')){
+ console.log(confirm('password cannot be null'));
+}
+else{
+ console.log(confirm('te dhenat jane rregull'));
+}
+
+
+
+
+
+}
